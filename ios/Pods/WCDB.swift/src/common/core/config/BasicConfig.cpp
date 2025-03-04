4de3d9d2e3b36ccead2a2b89ad69cae99017d575
Bolo <Bolo@guojiang.tv>
2025-03-04 18:00:33 +0800

    f5 pods

---
+++ ios/Pods/WCDB.swift/src/common/core/config/BasicConfig.cpp
@@ -0,0 +1,121 @@
+//
+// Created by sanhuazhang on 2019/05/02
+//
+
+/*
+ * Tencent is pleased to support the open source community by making
+ * WCDB available.
+ *
+ * Copyright (C) 2017 THL A29 Limited, a Tencent company.
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *       https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "BasicConfig.hpp"
+#include "Assertion.hpp"
+#include "CommonCore.hpp"
+#include "CoreConst.h"
+#include "InnerHandle.hpp"
+#include "Macro.h"
+
+namespace WCDB {
+
+BasicConfig::BasicConfig()
+: Config()
+, m_getJournalMode(StatementPragma().pragma(Pragma::journalMode()))
+, m_enableCheckPointFullfsync(
+  StatementPragma().pragma(Pragma::checkpointFullfsync()).to(true))
+, m_disableFullSync(StatementPragma().pragma(Pragma::synchronous()).to("OFF"))
+, m_setTempStore(StatementPragma().pragma(Pragma::tempStore()).to(1))
+{
+}
+
+BasicConfig::~BasicConfig() = default;
+
+bool BasicConfig::invoke(InnerHandle* handle)
+{
+    static_assert(SQLITE_DEFAULT_SYNCHRONOUS == 1, "");
+    static_assert(SQLITE_DEFAULT_WAL_SYNCHRONOUS == 1, "");
+    static_assert(SQLITE_DEFAULT_LOCKING_MODE == 0, "");
+
+    handle->enableExtendedResultCodes(true);
+    handle->disableCheckpointWhenClosing(true);
+    bool succeed = true;
+    if (!handle->isReadonly()) {
+        handle->setWALFilePersist(true);
+        succeed = lazySetJournalMode(handle);
+        if (!handle->liteModeEnable()) {
+            succeed = succeed && handle->execute(m_enableCheckPointFullfsync);
+        } else {
+            succeed = succeed && handle->execute(m_disableFullSync);
+        }
+#ifndef __ANDROID__
+        succeed = succeed && handle->execute(m_setTempStore);
+#endif
+    }
+    return succeed;
+}
+
+#pragma mark - Journal Mode
+bool BasicConfig::lazySetJournalMode(InnerHandle* handle)
+{
+    bool succeed = false;
+    handle->markErrorAsIgnorable(Error::Code::Busy);
+    int remainingNumberOfBusyRetryTimes = BasicConfigBusyRetryMaxAllowedNumberOfTimes;
+    do {
+        auto journalMode = getJournalMode(handle);
+        if (!journalMode.hasValue()) {
+            continue;
+        }
+        succeed = true;
+        if (!handle->liteModeEnable()) {
+            if (!journalMode.value().caseInsensitiveEqual("WAL")) {
+                if (!handle->canWriteMainDB()) {
+                    succeed = false;
+                    break;
+                } else {
+                    succeed = setJournalMode(handle, "WAL");
+                }
+            }
+        } else {
+            if (!journalMode.value().caseInsensitiveEqual("OFF")) {
+                succeed = setJournalMode(handle, "OFF");
+            }
+        }
+        // These is no busy handler for this busy event.
+        // So we should retry from outside.
+    } while (--remainingNumberOfBusyRetryTimes > 0 && !succeed
+             && handle->getError().isIgnorable());
+    handle->markErrorAsUnignorable();
+    return succeed;
+}
+
+Optional<StringView> BasicConfig::getJournalMode(InnerHandle* handle)
+{
+    Optional<StringView> ret;
+    if (handle->prepare(m_getJournalMode)) {
+        if (handle->step()) {
+            ret = handle->getText();
+        }
+        handle->finalize();
+    }
+    return ret;
+}
+
+bool BasicConfig::setJournalMode(InnerHandle* handle, const UnsafeStringView& mode)
+{
+    return handle->execute(StatementPragma().pragma(Pragma::journalMode()).to(mode));
+}
+
+} //namespace WCDB
---
+++ ios/Pods/WCDB.swift/src/common/core/config/BasicConfig.hpp
@@ -0,0 +1,60 @@
+//
+// Created by sanhuazhang on 2019/05/02
+//
+
+/*
+ * Tencent is pleased to support the open source community by making
+ * WCDB available.
+ *
+ * Copyright (C) 2017 THL A29 Limited, a Tencent company.
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *       https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "Config.hpp"
+#include "WINQ.h"
+
+namespace WCDB {
+
+class BasicConfig final : public Config {
+public:
+    BasicConfig();
+    ~BasicConfig() override;
+
+    bool invoke(InnerHandle* handle) override final;
+
+#pragma mark - Journal Mode
+protected:
+    bool lazySetJournalMode(InnerHandle* handle);
+
+private:
+    Optional<StringView> getJournalMode(InnerHandle* handle);
+    bool setJournalMode(InnerHandle* handle, const UnsafeStringView& mode);
+
+private:
+    const StatementPragma m_getJournalMode;
+
+#pragma mark - FullFsync
+protected:
+    const StatementPragma m_enableCheckPointFullfsync;
+    const StatementPragma m_disableFullSync;
+
+#pragma mark - Temp Store
+protected:
+    const StatementPragma m_setTempStore;
+};
+
+} //namespace WCDB
